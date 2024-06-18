458c05a0d8001ab7fb2c14992d47d68fbcc2e2b3
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-06-18 18:00:11 +0100

    Added more base files

--- tann-struct/node.py
+++ tann-struct/node.py
@@ -1,7 +1,7 @@
 class TrieNode():
-    def __init__(self, neural_network=None, feature_value=None):
+    def __init__(self, trie_network=None, feature_value=None):
         # Constructor for the TrieNode class.
         self.left_child = None  # Initialize left_child to None, this will hold the reference to the left child node.
         self.right_child = None  # Initialize right_child to None, this will hold the reference to the right child node.
-        self.neural_network = neural_network  # Assign the neural network passed to the node. This attribute stores the neural network associated with this node.
+        self.trie_network = trie_network  # Assign the neural network passed to the node. This attribute stores the neural network associated with this node.
         self.feature_value = feature_value
