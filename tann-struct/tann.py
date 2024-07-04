3a3466ed738af224941541787911b29b40ed7c37
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-07-04 13:06:09 +0100

    Update tann.py

--- tann-struct/tann.py
+++ tann-struct/tann.py
@@ -2,17 +2,39 @@ from trie import Trie
 from node import TrieNode
 
 class TANN():
-    def __init__(self, node=None, trie=None):
+    def __init__(self, trie=None):
         # Constructor method to initialize a TANN object with optional custom node and trie.
         # If no node or trie is provided, new instances of TrieNode and Trie will be created.
-        self.node = node if node is not None else TrieNode()
         self.trie = trie if trie is not None else Trie()
+        self.training_criteria = None
 
-    def train(self):
+    def train(self, train_data, epochs, lr):
         # Placeholder for the train method, which should be implemented to train the model.
         optimizers = []
-        nodes = self.trie.traverse_nodes()
-        pass
+        nodes = self.trie.traverse_nodes(self.trie.root)
+        for node in nodes:
+            optimizer = self.trie.neural_network.wrapper.get_optomizer(node.trie_network.parameters())
+            optimizers.append(optimizer)
+        
+        for epoch in range(epochs):
+            for inputs, target in train_data:
+                inputs = tensor(inputs, dtype=torch.float32)
+                target = tensor([target], dtype=torch.float32)
+
+            node = self.trie.root
+
+            #Implement training criteria
+
+            output = node.trie_network(inputs)
+            loss = self.node.trie_network.get_criterion(output, target)
+
+            for optimizer in optimizers:
+                optimizer.zero_grad()
+            loss.backward()
+            for optimizer in optimizers:
+                optimizer.step()
+
+            print(f"Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}")
 
     def inference(self):
         # Placeholder for the inference method, which should be implemented to process new data.
