cc52435b5adb4efc1ef26a107b704f3ddfdd1b0f
temitayoadefemi <111149469+temitayoadefemi@users.noreply.github.com>
2024-07-08 11:23:33 +0100

    Update neural_network.py

--- neural_network_struct/neural_network.py
+++ neural_network_struct/neural_network.py
@@ -4,7 +4,7 @@ class TrieNetwork():
     def __init__(self, library, input_size, hidden_size, output_size):
         self.wrapper = Wrapper(library=library, input_size=input_size, hidden_size=hidden_size, output_size=output_size)
 
-    def init_default_model(self):
+    def default_model(self):
         return self.wrapper.init_default_model()
     
     def get_criterion(self):
